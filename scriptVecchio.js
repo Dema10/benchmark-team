73bcb24ed2beec1f18f425e87902e6f693036879
Ilagen33 <ilariamontella162@gmail.com>
2024-03-06 00:00:23 +0100

    MOD

--- scriptVecchio.js
+++ scriptVecchio.js
@@ -98,29 +98,117 @@ const questions = [
     },
   ];
 
-  let div = document.querySelector(".risposte")
+  let rispostaGiusta = [];
+  for (let i = 0; i < questions.length; i++) {
+    rispostaGiusta.push(questions[i].correct_answer)
+    
+  }
+  console.log(rispostaGiusta);
+  
+  //PINO
+  
+  // let button = document.createElement(input);
+  // button.type = "radio";
+  // button.name = "answer";
+  // button.value = "answer";
+  // button.id = "input1";
+   let div = document.querySelector(".risposte")
+   let h1 = document.querySelector(".h1Domande");
+   let currentQuestionIndex = 0;
+   let score = 0;
 
-  // let currentQuestionIndex = 0;
-  // let score = 0;
-  // let question = 0;
-  let index = 0;
 
-  function checkObj(numberObj) {
-    // puntatore ad h1
-    let h1 = document.querySelector(".h1Domande");
-
-    for (let proprietà in questions[numberObj]) {
-      if (proprietà === 'question') {
-        h1.innerText = questions[numberObj][proprietà];
+  
+  document.addEventListener("DOMContentLoaded", function() {
+    for (let i = 0; i < questions.length; i++) {
+      h1.innerText = questions[i].question
+      if (questions.correct_answer == "True" || questions.correct_answer == "False") {
+        let button1 = document.createElement(input);
+         button1.type = "radio";
+         button1.name = "answer";
+         button1.value = "answer";
+         button1.id = "input1";
+         let button2 = document.createElement(input);
+         button2.type = "radio";
+         button2.name = "answer";
+         button2.value = "answer";
+         button2.id = "input2";
+      } else {
+        let button1 = document.createElement(input);
+         button1.type = "radio";
+         button1.name = "answer";
+         button1.value = "answer";
+         button1.id = "input1";
+         let button2 = document.createElement(input);
+         button2.type = "radio";
+         button2.name = "answer";
+         button2.value = "answer";
+         button2.id = "input2";
+         let button3 = document.createElement(input);
+         button3.type = "radio";
+         button3.name = "answer";
+         button3.value = "answer";
+         button3.id = "input3";
+         let button4 = document.createElement(input);
+         button4.type = "radio";
+         button4.name = "answer";
+         button4.value = "answer";
+         button4.id = "input4";
       }
     }
-    return 1
-}
+});
 
-index += checkObj(index);
+  // function loadQuestion() {
+  //   const currentQuestion = questions[currentQuestionIndex];
+
+  //   const questionContainer = document.createElement('div');
+  //   questionContainer.innerHTML = 
+  //     h1= `${currentQuestion.question}`
+  //     <form id="quizForm">
+  //       ${currentQuestion.incorrect_answers.map((answer, index) => 
+  //         <label>
+  //           <input type="radio" name="answer" value="${answer}">
+  //           ${answer}
+  //         </label>
+  //       ).join('')}
+  //       <label>
+  //         <input type="radio" name="answer" value="${currentQuestion.correct_answer}">
+  //         ${currentQuestion.correct_answer}
+  //       </label>
+  //       <br>
+  //       <input type="button" value="Next" onclick="checkAnswer()">
+  //     </form>
+  //   ;
+
+  //   document.body.innerHTML = '';
+  //   document.body.appendChild(questionContainer);
+  // }
+
+  // function checkAnswer() {
+  //   const selectedAnswer = document.querySelector('input[name="answer"]:checked');
+
+  //   if (selectedAnswer) {
+  //     if (selectedAnswer.value === questions[currentQuestionIndex].correct_answer) {
+  //       score++;
+  //     }
+
+  //     currentQuestionIndex++;
+
+  //     if (currentQuestionIndex < questions.length) {
+  //       loadQuestion();
+  //     } else {
+  //       console.log(Punteggio totale: ${score}/${questions.length});
+  //     }
+  //   }
+  // }
+
+  // // Inizia il quiz caricando la prima domanda
+  // loadQuestion();
 
 
-// const questionNumber = questions.length;
+  //ILARIA
+
+  // const questionNumber = questions.length;
 // const punteggio = 0;
 
 // let currentQuestion = 0;
