c446986bfe1cda7c2b110310ec253fd35ca52fe6
“Dema10” <gabrieledematteis@me.com>
2024-03-07 21:22:33 +0100

    coriandoli stella

--- scriptVecchio.js
+++ scriptVecchio.js
@@ -172,9 +172,15 @@ risposte.forEach(button => {
       if (index < questions.length) {
         // visualizza la prossima domanda
         checkObj(index)
-      } else if (score >= 6 && index === questions.length) {
-        setInterval(creareCoriandoli, 100);
+      } else if (score >= 6 && score < 10 && index === questions.length) {
+        setInterval(creareCoriandoli, 150);
         h1.innerText = "Congratulazioni, hai completato il test!"
+        div.innerHTML = ""
+        paragrafo.innerText = "Il tuo voto è " + score;
+        numeroTimer.style.display = "none";
+      } else if (score == 10 && index === questions.length) {
+        setInterval(creareCoriandoli2, 50);
+        h1.innerText = "Congratulazioni, hai completato il test! sei un top Player!"
         div.innerHTML = ""
         paragrafo.innerText = "Il tuo voto è " + score;
         numeroTimer.style.display = "none";
@@ -184,11 +190,6 @@ risposte.forEach(button => {
         paragrafo.innerText = "Il tuo voto è " + score
         numeroTimer.style.display = "none";
       }
-      /*if (score >= 6 && index === questions.length) {
-        setInterval(creareCoriandoli, 100);
-      }  else if (score < 6 && index === questions.length) {
-        paragrafo.innerText = "Sei stato bocciato!";     // non siamo riusciti a fare la condizione bocciato
-      }*/                                              // P.S sono anche le 04.00 PD non possiamo risolvere proprio tutto eh :P
     })
 });
 
@@ -232,18 +233,18 @@ function startTimer() {
           if (index < questions.length) {
               // visualizza la prossima domanda
               checkObj(index)
-          } else if (score >= 6 && index === questions.length) {
-              setInterval(creareCoriandoli, 100);
+            } else if (score >= 6 && score < 10 && index === questions.length) {
+              setInterval(creareCoriandoli, 150);
               h1.innerText = "Congratulazioni, hai completato il test!"
               div.innerHTML = ""
               paragrafo.innerText = "Il tuo voto è " + score;
               numeroTimer.style.display = "none";
-          } else {
+            } else {
               div.innerHTML = ""
               h1.innerText = "Mi dispiace, non hai passato il test."
               paragrafo.innerText = "Il tuo voto è " + score
               numeroTimer.style.display = "none";
-          }
+            }
       }
   }, 1000); // Ogni secondo
 
@@ -286,6 +287,38 @@ function removeTimer() {
 
   document.addEventListener('animationend', function () {
       document.body.removeChild(coriandoli);
+  });
+}
+
+function creareCoriandoli2() {
+  const coriandoliContainer = document.createElement("div");
+  coriandoliContainer.className = "coriandoli-container";
+
+  const coriandoli2 = document.createElement("div");
+  coriandoli2.className = "coriandoli2";
+
+  let inizioX = Math.random() * window.innerWidth;
+  let inizioY = Math.random() * window.innerHeight;
+  let forma = Math.random() * 15 + 10;
+  
+  coriandoli2.style.width = forma + 'px';
+  coriandoli2.style.height = forma + 'px';
+  coriandoli2.style.pointerEvents = 'none';
+  coriandoli2.style.opacity = 0.8;
+
+  coriandoliContainer.style.position = 'absolute';
+  coriandoliContainer.style.left = inizioX + 'px';
+  coriandoliContainer.style.top = inizioY + 'px';
+
+  coriandoliContainer.innerHTML = `<svg width="47" height="46" viewBox="0 0 60 80" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M22.2044 1.55551C22.6143 0.569963 24.0104 0.569964 24.4203 1.55552L29.9874 14.9402C30.1602 15.3557 30.5509 15.6396 30.9994 15.6756L45.4494 16.834C46.5134 16.9193 46.9448 18.2471 46.1341 18.9415L35.1248 28.3722C34.7831 28.6649 34.6338 29.1242 34.7382 29.5619L38.1018 43.6626C38.3494 44.7009 37.2199 45.5215 36.309 44.9651L23.9379 37.4089C23.5538 37.1743 23.0709 37.1743 22.6868 37.4089L10.3157 44.9651C9.40478 45.5215 8.27528 44.7009 8.52295 43.6626L11.8865 29.5619C11.9909 29.1242 11.8416 28.6649 11.4999 28.3722L0.490575 18.9415C-0.320069 18.2471 0.111362 16.9193 1.17535 16.834L15.6253 15.6756C16.0738 15.6396 16.4645 15.3557 16.6374 14.9402L22.2044 1.55551Z" fill="#00FFFF"/>
+      </svg>`;
+
+  coriandoliContainer.appendChild(coriandoli2);
+  document.body.appendChild(coriandoliContainer);
+
+  coriandoliContainer.addEventListener('animationend', function () {
+      document.body.removeChild(coriandoliContainer);
   });
 }
 
