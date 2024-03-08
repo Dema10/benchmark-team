735134eac85174512cb4e0a60d48a67f5a589368
Ilagen33 <ilariamontella162@gmail.com>
2024-03-08 16:03:13 +0100

    style changes

--- scriptPagina1.js
+++ scriptPagina1.js
@@ -2,5 +2,25 @@ let check = document.getElementById("checkbox");
 let button = document.getElementById("nascondi");
 
 checkbox.addEventListener("click", function () {
-  button.classList.toggle("procedi")
-})
+  button.classList.toggle("procedi");
+  toggleAnimation();
+});
+
+function toggleAnimation() {
+  var button = document.getElementById('nascondi');
+
+  // Rimuovi le classi di animazione precedenti
+  button.classList.remove('procedi-apri', 'procedi-chiudi');
+
+  // Aggiungi la classe procedi-rotola per far apparire il bottone rotolando
+  button.classList.add('procedi-rotola');
+
+  // Ascolta l'evento di fine animazione 'animationend'
+  button.addEventListener('animationend', function() {
+      // Rimuovi la classe procedi-rotola
+      button.classList.remove('procedi-rotola');
+
+      // Aggiungi la classe procedi-pulsa per far pulsare il bottone
+      button.classList.add('procedi-pulsa');
+  });
+};
